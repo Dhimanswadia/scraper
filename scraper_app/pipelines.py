3d6bddffab65bcb9d742d2fdb1fa6f691c0ca6b0
Dhiman Swadia <dhimanswadiachelsea@gmail.com>
2016-02-12 23:45:14 -0500

    SAS

--- scraper_app/pipelines.py
+++ scraper_app/pipelines.py
@@ -23,7 +23,6 @@ class LivingSocialPipeline(object):
 
         """
         engine = db_connect()
-        engine = instance
         create_deals_table(engine)
         self.Session = sessionmaker(bind=engine)
 
