742121e30aabd5af7953423886f46fa581ea2d12
Dhiman Swadia <dhimanswadiachelsea@gmail.com>
2016-02-12 22:32:11 -0500

    C9

--- scraper_app/settings.py
+++ scraper_app/settings.py
@@ -14,9 +14,9 @@ SPIDER_MODULES = ['scraper_app.spiders']
 ITEM_PIPELINES = ['scraper_app.pipelines.LivingSocialPipeline']
 
 DATABASE = {
-    'default': dj_database_url.config()
+    'default': dj_database_url.config(),
     #'drivername': 'postgres',
-  	'host':'ec2-54-225-223-40.compute-1.amazonaws.com' 
+  	'host':'ec2-54-225-223-40.compute-1.amazonaws.com',
     'port':'5432',
     'username':'juagqzmyvhljvz',  # fill in your username here
     'password':'H0GhC2nid4TcwYp1B76HMuQpFk',  # fill in your password here
